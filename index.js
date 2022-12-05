cbed3e10a454dd6d1231d113aada19fd96c3a143
Adam Romero <aromero0904@gmail.com>
2022-12-04 23:48:51 -0500

    commit tree d23901d734a196361c482d0d27415935e0407a8a

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('Hee Hee, that tickles!');
+}
+
+input.addEventListener('click', clickAlert);
 }
